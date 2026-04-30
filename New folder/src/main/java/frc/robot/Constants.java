e0a4ad244e099e6e655f2144529656a288873dd8
cybercubs3408 <robotics@loyolahs.edu>
2026-04-30 16:58:18 -0700

    first commit

---
+++ New folder/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Constants {
+    public static class motorConstants{
+        public static final int kShooterLeaderId=0;
+        public static final int kShooterFollowerId=1;
+        public static final int kShooterSpeed=1;
+    }
+}
---
+++ New folder/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
+import frc.robot.Constants.motorConstants;
+
+public class ShooterSubsystem extends SubsystemBase 
+{
+  public TalonFX m_leader;
+  public TalonFX m_follower;
+  /** Creates a new ExampleSubsystem. */
+  public ShooterSubsystem()
+   {
+   m_leader= new TalonFX(motorConstants.kShooterLeaderId); /**add correct device id's later */
+   m_follower= new TalonFX(motorConstants.kShooterFollowerId);
+   m_follower.setControl(new Follower(motorConstants.kShooterLeaderId, MotorAlignmentValue.Opposed));
+    
+
+  }
+  public void setShooterSpeed(double speed)
+  {
+    m_leader.set(speed);
+  }
+
+  public void stopShooter()
+  {
+    m_leader.set(0);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command Shoot()
+  {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return this.startEnd(
+      ()->{
+        setShooterSpeed(motorConstants.kShooterSpeed);
+      },
+      () -> {
+        stopShooter();
+      }
+
+    );
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
